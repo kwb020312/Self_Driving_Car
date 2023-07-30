47d245948f685e178de8040909bc264fe35b5e21
김우빈 <kwb020312@naver.com>
2023-07-30 20:26:30 +0900

    Feat: 차량 센서 추가 및 장애물 감지

--- car.js
+++ car.js
@@ -11,18 +11,19 @@ class Car {
     this.friction = 0.05;
     this.angle = 0;
 
+    this.sensor = new Sensor(this);
     this.controls = new Controls();
   }
 
-  update() {
+  update(roadBorders) {
     this.#move();
+    this.sensor.update(roadBorders);
   }
 
   #move() {
     if (this.controls.forward) {
       this.speed += this.acceleration;
     }
-
     if (this.controls.reverse) {
       this.speed -= this.acceleration;
     }
@@ -30,7 +31,6 @@ class Car {
     if (this.speed > this.maxSpeed) {
       this.speed = this.maxSpeed;
     }
-
     if (this.speed < -this.maxSpeed / 2) {
       this.speed = -this.maxSpeed / 2;
     }
@@ -38,22 +38,18 @@ class Car {
     if (this.speed > 0) {
       this.speed -= this.friction;
     }
-
     if (this.speed < 0) {
       this.speed += this.friction;
     }
-
     if (Math.abs(this.speed) < this.friction) {
       this.speed = 0;
     }
 
     if (this.speed != 0) {
       const flip = this.speed > 0 ? 1 : -1;
-
       if (this.controls.left) {
         this.angle += 0.03 * flip;
       }
-
       if (this.controls.right) {
         this.angle -= 0.03 * flip;
       }
@@ -61,17 +57,19 @@ class Car {
 
     this.x -= Math.sin(this.angle) * this.speed;
     this.y -= Math.cos(this.angle) * this.speed;
-    this.y -= this.speed;
   }
 
   draw(ctx) {
     ctx.save();
     ctx.translate(this.x, this.y);
     ctx.rotate(-this.angle);
+
     ctx.beginPath();
     ctx.rect(-this.width / 2, -this.height / 2, this.width, this.height);
     ctx.fill();
 
     ctx.restore();
+
+    this.sensor.draw(ctx);
   }
 }
