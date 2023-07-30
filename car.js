73697fc42cb73e55f199ed64540b16182f13c5d4
김우빈 <kwb020312@naver.com>
2023-07-30 17:55:51 +0900

    Feat: 자동차 회전 각 적용

--- car.js
+++ car.js
@@ -9,11 +9,16 @@ class Car {
     this.acceleration = 0.2;
     this.maxSpeed = 3;
     this.friction = 0.05;
+    this.angle = 0;
 
     this.controls = new Controls();
   }
 
   update() {
+    this.#move();
+  }
+
+  #move() {
     if (this.controls.forward) {
       this.speed += this.acceleration;
     }
@@ -42,24 +47,31 @@ class Car {
       this.speed = 0;
     }
 
-    if (this.controls.left) {
-      this.x -= 2;
+    if (this.speed != 0) {
+      const flip = this.speed > 0 ? 1 : -1;
+
+      if (this.controls.left) {
+        this.angle += 0.03 * flip;
+      }
+
+      if (this.controls.right) {
+        this.angle -= 0.03 * flip;
+      }
     }
 
-    if (this.controls.right) {
-      this.x += 2;
-    }
+    this.x -= Math.sin(this.angle) * this.speed;
+    this.y -= Math.cos(this.angle) * this.speed;
     this.y -= this.speed;
   }
 
   draw(ctx) {
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(-this.angle);
     ctx.beginPath();
-    ctx.rect(
-      this.x - this.width / 2,
-      this.y - this.height / 2,
-      this.width,
-      this.height
-    );
+    ctx.rect(-this.width / 2, -this.height / 2, this.width, this.height);
     ctx.fill();
+
+    ctx.restore();
   }
 }
