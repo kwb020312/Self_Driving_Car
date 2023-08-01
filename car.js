6083bfef9e7f1cac8c9fbf15be8fe2a3b0a07f2e
김우빈 <kwb020312@naver.com>
2023-08-01 20:58:11 +0900

    Feat: 최적의 Neural 저장 및 불러오기

--- car.js
+++ car.js
@@ -126,7 +126,7 @@ class Car {
     this.y -= Math.cos(this.angle) * this.speed;
   }
 
-  draw(ctx, color) {
+  draw(ctx, color, drawSensor = false) {
     if (this.damaged) {
       ctx.fillStyle = "gray";
     } else {
@@ -139,6 +139,6 @@ class Car {
     }
     ctx.fill();
 
-    if (this.sensor) this.sensor.draw(ctx);
+    if (this.sensor && drawSensor) this.sensor.draw(ctx);
   }
 }
